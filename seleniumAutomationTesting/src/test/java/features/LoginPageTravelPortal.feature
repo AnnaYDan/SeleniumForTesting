d85d1490a70e8fe6a882e6558c65b1d861f99a7e
TOSHIBA <TOSHIBA@TOSHIBA>
2018-08-01 18:40:48 -0400

    update 08/01/2018

--- seleniumAutomationTesting/src/test/java/features/LoginPageTravelPortal.feature
+++ seleniumAutomationTesting/src/test/java/features/LoginPageTravelPortal.feature
@@ -5,15 +5,16 @@ Feature: Test cases for Travel Portal login page with different credentials by A
     When I insert username = "<name>" and password ="<pass>"
     And click login button
     Then "<link>" link should be displayed
+    And page title should be "<title>" 
 
     Examples: 
-      | name  | pass  | link     |
-      | a     | a     | SIGN-OFF |
-      | a     | wrong | SIGN-ON  |
-      | a     |       | SIGN-ON  |
-      |       | a     | SIGN-ON  |
-      |       |       | SIGN-ON  |
-      |       | wrong | SIGN-ON  |
-      | wrong | wrong | SIGN-ON  |
-      | wrong | a     | SIGN-ON  |
-      | wrong |       | SIGN-ON  |
+      | name  | pass  | link     | title |
+      | a     | a     | SIGN-OFF | |
+      | a     | wrong | SIGN-ON  ||
+      | a     |       | SIGN-ON  ||
+      |       | a     | SIGN-ON  ||
+      |       |       | SIGN-ON  ||
+      |       | wrong | SIGN-ON  ||
+      | wrong | wrong | SIGN-ON  ||
+      | wrong | a     | SIGN-ON  ||
+      | wrong |       | SIGN-ON  ||
--- seleniumAutomationTesting/src/test/java/testNGexamples/ManageAlertsTestNG.java
+++ seleniumAutomationTesting/src/test/java/testNGexamples/ManageAlertsTestNG.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -20,6 +21,7 @@ public class ManageAlertsTestNG {
 		driver.get("http://www.training.qaonlinetraining.com/testPage.php");
 		driver.findElement(By.id("prompt")).click();
 		driver.switchTo().alert().sendKeys("Konstantinopolsky");
+		System.out.println(">>>>>>>>>>>>>>>> before test finished");		
 	}
 	
 	// first test for accept alert
@@ -28,6 +30,7 @@ public class ManageAlertsTestNG {
 		driver.switchTo().alert().accept();
 		String demo = driver.findElement(By.id("demo")).getText();
 		Assert.assertTrue(demo.contains("Konstantinopolsky"));
+		System.out.println("*************** test with accept finished *************");		
 	}
 	
 	// second test for dismiss alert
@@ -37,12 +40,20 @@ public class ManageAlertsTestNG {
 		String demo = driver.findElement(By.id("demo")).getText();
 		//Assert.assertTrue(!demo.contains("Konstantinopolsky"));
 		Assert.assertFalse(demo.contains("Konstantinopolsky"));
+		System.out.println("*************** test with dismiss finished *************");		
 	}
 
 	// running after executing test method with just printing about this fact
 	@AfterMethod
 	public void afterMethod() {
+		//driver.quit();
 		System.out.println("<<<<<<<<<<<<<<<< after test finished");		
 	}
 
+	// running after executing test method with just printing about this fact
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+		System.out.println("<<<<<<<<<<<<<<<< after class finished");		
+	}
 }
